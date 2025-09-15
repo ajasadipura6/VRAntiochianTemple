e202ce011a7d333324895a306ab704f604a0d9a3
coopergrant1 <coopergrant19@gmail.com>
2025-09-14 23:01:12 -0400

    Build data, disk size minimizations
    Working on optimizing build size for itch.io publish limit, removed unnecessary reconstruction block objects, removed more detailed ground site plan under temple for now

---
+++ New VR Project/Assets/Blocks/AT119 Block/BlockInfoRecon.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BlockInfoRecon : MonoBehaviour
+{
+    public GameObject blockRecon; // Assign the block reconstruction in the Inspector
+    public GameObject pdfWindow; // Assign the PDF display panel in the Inspector
+
+    private bool isVisible = false;
+
+    void Start()
+    {
+        blockRecon.SetActive(false);   // Initially hide the block reconstruction
+        pdfWindow.SetActive(false);   // Initially hide the PDF window
+    }
+
+    public void TogglePDF()
+    {
+        isVisible = !isVisible;
+        blockRecon.SetActive(isVisible);
+        pdfWindow.SetActive(isVisible);
+    }
+}
--- New VR Project/Assets/Scripts/BlockInfo.cs
+++ New VR Project/Assets/Scripts/BlockInfo.cs
@@ -2,21 +2,18 @@ using UnityEngine;
 
 public class BlockInfo : MonoBehaviour
 {
-    public GameObject blockRecon; // Assign the block reconstruction in the Inspector
     public GameObject pdfWindow; // Assign the PDF display panel in the Inspector
 
     private bool isVisible = false;
 
     void Start()
     {
-        blockRecon.SetActive(false);   // Initially hide the block reconstruction
         pdfWindow.SetActive(false);   // Initially hide the PDF window
     }
 
     public void TogglePDF()
     {
         isVisible = !isVisible;
-        blockRecon.SetActive(isVisible);
         pdfWindow.SetActive(isVisible);
     }
 }
